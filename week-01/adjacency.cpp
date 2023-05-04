1d196486e2bde8c687324639b39a03523cc5b9e5
sajjadhussains <shuvosajjad70@gmail.com>
2023-05-05 04:46:10 +0600

    After completing adjacency matrix representation

--- week-01/adjacency.cpp
+++ week-01/adjacency.cpp
@@ -24,6 +24,10 @@
     3->2
     3->1
     1->3
+
+    Time Complexity->O(n^2)
+    Space Complexity->O(n^2)
+
 */
 
 #include<bits/stdc++.h>
@@ -33,16 +37,29 @@ using namespace std;
 int main(){
     int nodes=4;
 
-    int matrix[nodes][nodes];
+    int matrix[nodes][nodes]={};
+
+    matrix[1][0]= 1;
+    matrix[0][1]= 1;
+
+    matrix[1][2] = 1;
+    matrix[2][1] = 1;
+
+    matrix[2][3] = 1;
+    matrix[3][2] = 1;
+
+    matrix[3][1] = 1;
+    matrix[1][3] = 1;
+
+    // for(int i=0;i<nodes;i++){
+    //     for(int j=0;j<nodes;j++){
+    //         matrix[i][j]=0;
+    //     }
+    // }
 
     for(int i=0;i<nodes;i++){
         for(int j=0;j<nodes;j++){
-            matrix[i][j]=0;
-        }
-    }
-
-    for(int i=0;i<nodes;i++){
-        for(int j=0;j<nodes;j++){
+            // cout<<matrix[i][j]<<"("<<i<<","<<j<<")"<<" ";
             cout<<matrix[i][j]<<" ";
         }
         cout<<endl;
