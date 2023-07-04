02cdca984887af1995f70adb68267bacfcf4b159
sajjadhussains <shuvosajjad70@gmail.com>
2023-07-04 07:56:12 +0600

    after practicing single source shortest path

---
+++ week-03/sssp.cpp
@@ -0,0 +1,52 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int const N = 1e5;
+vector<int> adj_list[N];
+int visited[N];
+int level[N];
+
+void bfs(int src){
+    queue<int>q;
+    visited[src]=1;
+    level[src] = 0;
+    q.push(src);
+
+    while(!q.empty()){
+        int head = q.front();
+        q.pop();
+
+        // cout<<head<<endl;
+        for(int adj_node:adj_list[head]){
+            if(visited[adj_node]==0){
+                visited[adj_node]=1;
+                level[adj_node]=level[head]+1;
+                q.push(adj_node);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int nodes, edges;
+    cin >> nodes >> edges;
+
+    for (int i = 0; i < edges; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+
+    int src = 0;
+    bfs(src);
+
+    for(int i=0;i<nodes;i++){
+        cout<<"node "<<i<<": level->"<<level[i]<<endl;
+    }
+    return 0;
+}
