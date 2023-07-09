ba247f9a0b9afb87b0b18d71c2fdf3ece2138c6f
sajjadhussains <shuvosajjad70@gmail.com>
2023-07-09 11:57:25 +0600

    two problem solved

---
+++ week-03/topological_sorting.cpp
@@ -0,0 +1,46 @@
+// problem link:https://cses.fi/problemset/task/1679
+
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 2e5;
+
+vector<int>adj_list[N];
+int visited[N];
+stack<int>node_stack;
+
+void dfs(int node){
+    visited[node]=1;
+
+    for(int adj_node:adj_list[node]){
+        if(visited[adj_node] == 0){
+            dfs(adj_node);
+        }
+    }
+    node_stack.push(node);
+}
+int main()
+{
+    int nodes,edges;
+    cin>>nodes>>edges;
+
+    for(int i=0;i<edges;i++){
+        int u,v;
+        cin>>u>>v;
+
+        adj_list[u].push_back(v);
+    }
+
+    for(int i=1;i<=nodes;i++){
+        if(visited[i]==0){
+            dfs(i);
+        }
+    }
+    while (!node_stack.empty())
+    {
+        cout<<node_stack.top()<<" ";
+        node_stack.pop();
+    }
+    cout<<endl;
+
+    return 0;
+}
